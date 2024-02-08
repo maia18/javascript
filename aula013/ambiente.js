9a4921a881ac511fa17b1341e11fa2e531d2c64d
maia18 <nelsonkaue05100@gmail.com>
2024-02-08 19:19:47 -0300

    xe

--- aula013/ambiente.js
+++ aula013/ambiente.js
@@ -1,6 +1,18 @@
 var c = 1
 
-do {
-    console.log('Tudo bem? ')
-    c++ // c = c + 1
-} while ( c <= 6)
+// do {
+//     console.log('Tudo bem? ')
+//     c++ // c = c + 1
+// } while ( c <= 6 )
+
+// while ( c <= 6 ) {
+//     console.log('Tudo bem? ')
+//     c++
+// }
+
+// console.log('Tudo bem? ')
+// console.log('Tudo bem? ')
+// console.log('Tudo bem? ')
+// console.log('Tudo bem? ')
+// console.log('Tudo bem? ')
+// console.log('Tudo bem? ')

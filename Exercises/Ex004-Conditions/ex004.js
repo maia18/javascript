52ac7a5627ebe1d47f605c0be7afe5e351d56505
maia18 <nelsonkaue05100@gmail.com>
2024-05-31 17:25:55 -0300

    att

---
+++ Exercises/Ex004-Conditions/ex004.js
@@ -0,0 +1,5 @@
+var vel = 60.5
+console.log(`Velocity car: ${vel}km/h`)
+if (vel > 60) {
+    console.log('Slow Down!')
+}
---
+++ Exercises/Ex004-Conditions/ex004_1/ex004_1.js
@@ -0,0 +1,7 @@
+var country = 'USA'
+console.log(`Living in ${country}`)
+if (country != 'BR') {
+    console.log("You are foreigner")
+} else {
+    console.log("You are Brazilian!")
+}

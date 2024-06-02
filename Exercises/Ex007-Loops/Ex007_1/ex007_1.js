64574ff40c2a8f178b3866aea4dfe792898f0c32
maia18 <nelsonkaue05100@gmail.com>
2024-06-02 18:57:45 -0300

    att

---
+++ Exercises/Ex007-Loops/Ex007_1/ex007_1.js
@@ -0,0 +1,5 @@
+console.log("It'll begin...")
+for (var c = 1; c <= 10; c++) {
+    console.log(`${c}º`)
+}
+console.log("End")
---
+++ Exercises/Ex007-Loops/ex007.js
@@ -0,0 +1,19 @@
+/* console.log("Hello, world!")
+console.log("Hello, world!")
+console.log("Hello, world!")
+console.log("Hello, world!")
+console.log("Hello, world!")
+console.log("Hello, world!")
+console.log("Hello, world!") */
+
+/* var c = 1 
+while (c <= 7) {
+    console.log(`${c}º - Hello, world!`)
+    c++
+} */
+
+var c = 1 
+do {
+    console.log(`${c}º - Hello, world!`)
+    c++
+} while (c <= 7)

938193a98874d4e5b1951df4ba97cfddcd6783fd
maia18 <nelsonkaue05100@gmail.com>
2024-06-04 20:55:45 -0300

    att

--- Exercises/Ex012-Practicing_5/JS/analyze.js
+++ Exercises/Ex012-Practicing_5/JS/analyze.js
@@ -1,20 +1,56 @@
-function ANALYZER() {
-    var num = document.querySelector("input#Inum")
-    var ul = document.getElementById("list")
-    var res = document.querySelector("div#res")
+var num = document.getElementById("Inum")
+var res = document.querySelector("div#res")
 
-    var list = []
+let select = document.querySelector("select#inumbers")
 
-    let item = document.createElement("li")
-    ul.appendChild(item)
+let list_ = []
 
-    for (var obj in ul) {
-        res.innerHTML += ul[obj]
+function ANALYZER_ITEM(item_, lists) {
+    if (lists.indexOf(Number(item_)) != -1) {
+        return true
+    } else {
+        return false
     }
+}
 
-    // list.push(ul)
+function ADD() {
 
-    // res.innerHTML += ul
-    // var letters;
-    // letters += "<li>" + list
-}  
+    if ( ! ANALYZER_ITEM(num.value, list_)) {
+        list_.push(Number(num.value))
+
+        let item = document.createElement("option")
+        item.text = `${num.value} added!`
+        select.appendChild(item)
+
+        res.innerHTML = ''
+    } else {
+        alert(`Invalid datas!`)
+    }
+    num.value = ''
+    num.focus()
+}
+
+function ANALYZER() {
+    if (list_.length == 0) {
+        window.alert("Add items bafore finishing!")
+    } else {
+        let tot = list_.length
+        let biggest = Math.max(...list_)
+        let lowerest = Math.min(...list_)
+        var sum = 0
+        var average = 0
+
+        for (var pos_ in list_) {
+            sum += list_[pos_]
+        } 
+
+        average = sum / tot
+
+        res.innerHTML = ''
+        res.innerHTML += `<p>There are <strong>${tot}</strong> items</p>`
+        res.innerHTML += `<p>The biggest value is <strong>${biggest}</strong></p>`
+        res.innerHTML += `<p>The lowerest value is <strong>${lowerest}</strong></p>`
+        res.innerHTML += `<p>The Sum is <strong>${sum}</strong></p>`
+        res.innerHTML += `<p>The Average is <strong>${average}</strong></p>`
+    }
+}

340cb5a88088f75fb5344acd3ed3cfb3a23b152d
maia18 <nelsonkaue05100@gmail.com>
2024-06-15 12:59:06 -0300

    t

--- Exercises/Ex007-Loops/Ex007_1/ex007_1.js
+++ Exercises/Ex007-Loops/Ex007_1/ex007_1.js
@@ -1,5 +1,20 @@
-console.log("It'll begin...")
-for (var c = 1; c <= 10; c++) {
-    console.log(`${c}º`)
+// console.log("It'll begin...")
+// for (var c = 1; c <= 10; c++) {
+//     console.log(`${c}º`)
+// }
+// console.log("End")
+
+// for (let i = 10; i >= 2; i-=2) {
+//     console.log(`${i}º - Hello world!`);
+// }
+
+// for (let i = 10; i >= 0; i--) {
+//     const even = i % 2 === 0 ? 'even' : 'odd';
+//     console.log(`${i} - ${even}`);
+// }
+
+const fruits = ['apple', 'peach', 'grape', 'strawberry', 'banana', 'watermelon'];
+
+for (let i = 0; i < fruits.length; i ++) {
+    console.log(`${i}º fruit : ${fruits[i].toUpperCase()}`)
 }
-console.log("End")

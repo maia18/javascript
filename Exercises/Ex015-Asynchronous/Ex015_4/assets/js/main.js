cda855df08165c542e21ee0e6d599d472b86d767
maia18 <nelsonkaue05100@gmail.com>
2024-07-04 17:35:45 -0300

    Update main.js

--- Exercises/Ex015-Asynchronous/Ex015_4/assets/js/main.js
+++ Exercises/Ex015-Asynchronous/Ex015_4/assets/js/main.js
@@ -1,23 +1,26 @@
-fetch('pessoas.json')
-  .then(resposta => resposta.json())
-  .then(json => carregaElementosNaPagina(json));
+// fetch('pessoas.json')
+// .then(answer => answer.json())
+// .then(json => loadElementsPage(json));
 
-// axios('pessoas.json')
-//   .then(resposta => carregaElementosNaPagina(resposta.data));
+axios('pessoas.json')
+.then(answer => loadElementsPage(answer.data))
 
-function carregaElementosNaPagina(json) {
+function loadElementsPage(json) {
   const table = document.createElement('table');
-
-  for(let pessoa of json) {
+  for (let people of json) {
     const tr = document.createElement('tr');
 
-    let td1 = document.createElement('td');
-    td1.innerHTML = pessoa.nome;
-    tr.appendChild(td1);
+    let td_1 = document.createElement('td');
+    td_1.innerHTML = people.nome;
+    tr.appendChild(td_1);
 
-    let td2 = document.createElement('td');
-    td2.innerHTML = pessoa.idade;
-    tr.appendChild(td2);
+    let td_2 = document.createElement('td');
+    td_2.innerHTML = people.idade;
+    tr.appendChild(td_2);
+
+    let td_3 = document.createElement('td');
+    td_3.innerHTML = people.salario;
+    tr.appendChild(td_3);
 
     table.appendChild(tr);
   }

d62eba8dfa2d9841e568ff7d6bef0851c7386eb1
maia18 <nelsonkaue05100@gmail.com>
2024-07-05 11:50:26 -0300

    att

---
+++ Exercises/Ex016-Modules/main.js
@@ -0,0 +1,12 @@
+class Person {
+    constructor(name, last_name) {
+        this.name = name;
+        this.last_name = last_name;
+    }
+}
+/*
+
+ https://caniuse.com/
+ https://babeljs.io/
+
+*/

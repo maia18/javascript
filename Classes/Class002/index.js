ccab49d1881e68102669514f6dc8cd4ba2155ded
maia18 <nelsonkaue05100@gmail.com>
2024-12-23 10:52:36 -0300

    estudos

---
+++ Classes/Class002/index.js
@@ -0,0 +1,5 @@
+console.log("Lorrane 'top' "); // Strings = Textos
+console.log('Carlos "crazy" ');
+console.log(`Cleiton "caminhoneiro" e 'massa' `);
+
+console.log(123, 7.6, 3.1456); // Numbers
---
+++ Classes/Class003/comments.js
@@ -0,0 +1,23 @@
+/* 
+Escrever um comentário aqui 
+(possivelmente longo) em formato de bloco
+ */
+
+console.log("Hello, world!"); // E/ou aqui também
+console.log("Linha 1");
+
+/** Ou assim: 
+ * Título
+ * Data
+ * Autor
+ * Valor
+*/
+
+// console.log("Linha 2"); // ----> Linha Ignorada
+
+/* 
+console.log("Linha 3"); 
+console.log("Linha 4");
+*/ // ----> Linhas Ignorada
+
+// Será ignorado
